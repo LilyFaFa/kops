274c97d46b9dd6d5122a56e5641b87f1a296ff72
LilyFaFa <21621231@zju.edu.cn>
2018-01-20 22:12:59 +0800

    add loabalancer model

---
+++ pkg/model/alimodel/api_loadbalancer.go
@@ -0,0 +1,131 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package alimodel
+
+import (
+	"fmt"
+
+	"k8s.io/kops/pkg/apis/kops"
+	"k8s.io/kops/pkg/dns"
+	"k8s.io/kops/upup/pkg/fi"
+	"k8s.io/kops/upup/pkg/fi/cloudup/alitasks"
+	"k8s.io/kops/upup/pkg/fi/fitasks"
+)
+
+const LoadBalancerListenerStatus = "running"
+const LoadBalancerListenerBandwidth = -1
+
+// APILoadBalancerModelBuilder builds a LoadBalancer for accessing the API
+type APILoadBalancerModelBuilder struct {
+	*ALIModelContext
+	Lifecycle *fi.Lifecycle
+}
+
+var _ fi.ModelBuilder = &APILoadBalancerModelBuilder{}
+
+func (b *APILoadBalancerModelBuilder) Build(c *fi.ModelBuilderContext) error {
+	// Configuration where an ELB fronts the API
+	if !b.UseLoadBalancerForAPI() {
+		return nil
+	}
+
+	lbSpec := b.Cluster.Spec.API.LoadBalancer
+	if lbSpec == nil {
+		// Skipping API ELB creation; not requested in Spec
+		return nil
+	}
+
+	switch lbSpec.Type {
+	case kops.LoadBalancerTypeInternal:
+		// OK
+	case kops.LoadBalancerTypePublic:
+		return fmt.Errorf("internal LoadBalancers are not yet supported by kops on ALI")
+	default:
+		return fmt.Errorf("unhandled LoadBalancer type %q", lbSpec.Type)
+	}
+
+	// Create LoadBalancer for API ELB
+	var loadbalancer *alitasks.LoadBalancer
+	{
+
+		loadbalancer = &alitasks.LoadBalancer{
+			Name:      s("api." + b.ClusterName()),
+			Lifecycle: b.Lifecycle,
+		}
+
+		switch lbSpec.Type {
+		case kops.LoadBalancerTypeInternal:
+			return fmt.Errorf("Don't support internal loadbalancer type: %q", lbSpec.Type)
+		case kops.LoadBalancerTypePublic:
+			loadbalancer.AddressType = s("internet")
+		default:
+			return fmt.Errorf("unknown loadbalancer Type: %q", lbSpec.Type)
+		}
+
+		c.AddTask(loadbalancer)
+	}
+
+	// Create LoadBalancerListener for API ELB
+	// TODO: Health check
+	var loadbalancerlistener *alitasks.LoadBalancerListener
+	{
+		loadbalancerlistener = &alitasks.LoadBalancerListener{
+			Name:              s("api." + b.ClusterName()),
+			Lifecycle:         b.Lifecycle,
+			LoadBalancer:      loadbalancer,
+			ListenerStatus:    s(LoadBalancerListenerStatus),
+			ListenerPort:      i(443),
+			BackendServerPort: i(443),
+			Bandwidth:         i(LoadBalancerListenerBandwidth),
+		}
+		c.AddTask(loadbalancerlistener)
+	}
+
+	// Create LoadBalancerWhiteList for API ELB
+	var loadbalancerwhiteList *alitasks.LoadBalancerWhiteList
+	{
+		sourceItems := ""
+		for _, cidr := range b.Cluster.Spec.KubernetesAPIAccess {
+			sourceItems = sourceItems + cidr + ","
+		}
+		loadbalancerwhiteList = &alitasks.LoadBalancerWhiteList{
+			Name:                 s("api." + b.ClusterName()),
+			Lifecycle:            b.Lifecycle,
+			LoadBalancer:         loadbalancer,
+			LoadBalancerListener: loadbalancerlistener,
+			SourceItems:          s(sourceItems),
+		}
+		c.AddTask(loadbalancerwhiteList)
+	}
+
+	// TODO: Should this be removed?
+	// Temporarily do not know the role of the following function
+	if dns.IsGossipHostname(b.Cluster.Name) || b.UsePrivateDNS() {
+		// Ensure the ELB hostname is included in the TLS certificate,
+		// if we're not going to use an alias for it
+		// TODO: I don't love this technique for finding the task by name & modifying it
+		masterKeypairTask, found := c.Tasks["Keypair/master"]
+		if !found {
+			return fmt.Errorf("keypair/master task not found")
+		}
+		masterKeypair := masterKeypairTask.(*fitasks.Keypair)
+		masterKeypair.AlternateNameTasks = append(masterKeypair.AlternateNameTasks, loadbalancer)
+	}
+
+	return nil
+
+}
---
+++ upup/pkg/fi/cloudup/alitasks/loadbalancerwhitelist.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package alitasks
+
+import (
+	"fmt"
+	"strings"
+
+	"k8s.io/kops/upup/pkg/fi"
+	"k8s.io/kops/upup/pkg/fi/cloudup/aliup"
+)
+
+//"k8s.io/kops/upup/pkg/fi/cloudup/terraform"
+
+//go:generate fitask -type=LoadBalancerWhiteList
+type LoadBalancerWhiteList struct {
+	LoadBalancer         *LoadBalancer
+	LoadBalancerListener *LoadBalancerListener
+	Name                 *string
+	SourceItems          *string
+	Lifecycle            *fi.Lifecycle
+}
+
+var _ fi.CompareWithID = &LoadBalancerWhiteList{}
+
+func (l *LoadBalancerWhiteList) CompareWithID() *string {
+	return l.Name
+}
+
+func (l *LoadBalancerWhiteList) Find(c *fi.Context) (*LoadBalancerWhiteList, error) {
+	if l.LoadBalancer.LoadbalancerId == nil {
+		return nil, fmt.Errorf("error finding LoadBalancerWhiteList, lack of LoadBalancerId")
+	}
+	if l.LoadBalancerListener.ListenerPort == nil {
+		return nil, fmt.Errorf("error finding LoadBalancerWhiteList, lack of ListenerPort")
+	}
+	cloud := c.Cloud.(aliup.ALICloud)
+	loadBalancerId := fi.StringValue(l.LoadBalancer.LoadbalancerId)
+	listenertPort := fi.IntValue(l.LoadBalancerListener.ListenerPort)
+
+	response, err := cloud.SlbClient().DescribeListenerAccessControlAttribute(loadBalancerId, listenertPort)
+	if err != nil {
+		return nil, fmt.Errorf("error finding LoadBalancerWhiteList: %v", err)
+	}
+	if response.SourceItems == "" {
+		return nil, nil
+	}
+	actual := &LoadBalancerWhiteList{}
+	actual.SourceItems = fi.String(response.SourceItems)
+	// Ignore "system" fields
+	actual.LoadBalancer = l.LoadBalancer
+	actual.Lifecycle = l.Lifecycle
+
+	return actual, nil
+}
+
+func (l *LoadBalancerWhiteList) Run(c *fi.Context) error {
+	return fi.DefaultDeltaRunMethod(l, c)
+}
+
+func (_ *LoadBalancerWhiteList) CheckChanges(a, e, changes *LoadBalancerWhiteList) error {
+	if a == nil {
+		if e.Name == nil {
+			return fi.RequiredField("Name")
+		}
+		if e.LoadBalancer.LoadbalancerId == nil {
+			return fi.RequiredField("LoadBalnacerId")
+		}
+		if e.LoadBalancerListener.ListenerPort == nil {
+			return fi.RequiredField("ListenerPort")
+		}
+	}
+	return nil
+}
+
+//LoadBalancer can only modify tags.
+func (_ *LoadBalancerWhiteList) RenderALI(t *aliup.ALIAPITarget, a, e, changes *LoadBalancerWhiteList) error {
+	if e.LoadBalancer.LoadbalancerId == nil {
+		return fmt.Errorf("error creating LoadBalancerWhiteList, lack of LoadBalnacerId")
+	}
+	if e.LoadBalancerListener.ListenerPort == nil {
+		return fmt.Errorf("error creating LoadBalancerWhiteList, lack of ListenerPort")
+	}
+
+	loadBalancerId := fi.StringValue(e.LoadBalancer.LoadbalancerId)
+	listenertPort := fi.IntValue(e.LoadBalancerListener.ListenerPort)
+	sourceItems := fi.StringValue(e.SourceItems)
+	if sourceItems != "" {
+		err := t.Cloud.SlbClient().AddListenerWhiteListItem(loadBalancerId, listenertPort, sourceItems)
+		if err != nil {
+			return fmt.Errorf("error adding LoadBalancerWhiteListItems: %v", err)
+		}
+	}
+
+	if a != nil && changes.SourceItems != nil {
+		itemsToDelete := e.getWhiteItemsToDelete(fi.StringValue(a.SourceItems))
+		if itemsToDelete != "" {
+			err := t.Cloud.SlbClient().RemoveListenerWhiteListItem(loadBalancerId, listenertPort, itemsToDelete)
+			if err != nil {
+				return fmt.Errorf("error removing LoadBalancerWhiteListItems: %v", err)
+			}
+		}
+	}
+	return nil
+}
+
+func (l *LoadBalancerWhiteList) getWhiteItemsToDelete(currentWhiteItems string) string {
+	currentWhiteItemsList := strings.Split(currentWhiteItems, ",")
+	expectedWhiteItemsList := strings.Split(fi.StringValue(l.SourceItems), ",")
+	itemsToDelete := ""
+	for _, currentItem := range currentWhiteItemsList {
+		expected := false
+		if currentItem == "" {
+			continue
+		}
+		for _, expectedItem := range expectedWhiteItemsList {
+			if currentItem == expectedItem {
+				expected = true
+			}
+		}
+		if expected == false {
+			itemsToDelete = itemsToDelete + "," + currentItem
+		}
+	}
+	return itemsToDelete
+}
