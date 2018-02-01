3a6f1aead9990cc9eef7c3d84beb8a5fbb479dcf
LilyFaFa <21621231@zju.edu>
2018-02-01 20:41:34 +0800

    update aliyungo

--- vendor/github.com/denverdino/aliyungo/ess/group.go
+++ vendor/github.com/denverdino/aliyungo/ess/group.go
@@ -1,6 +1,9 @@
 package ess
 
-import "github.com/denverdino/aliyungo/common"
+import (
+	"github.com/denverdino/aliyungo/common"
+	"time"
+)
 
 type LifecycleState string
 
@@ -19,12 +22,13 @@ type CreateScalingGroupArgs struct {
 	LoadBalancerIds  string
 	VpcId            string
 	VSwitchId        string
+	VSwitchIds       common.FlattenArray
 	// NOTE: Set MinSize, MaxSize and DefaultCooldown type to int pointer to distinguish zero value from unset value.
 	MinSize         *int
 	MaxSize         *int
 	DefaultCooldown *int
 	RemovalPolicy   common.FlattenArray
-	DBInstanceId    common.FlattenArray
+	DBInstanceIds   string
 }
 
 type CreateScalingGroupResponse struct {
@@ -95,6 +99,7 @@ type ScalingGroupItemType struct {
 	RegionId                     string
 	LoadBalancerId               string
 	VSwitchId                    string
+	VSwitchIds                   VSwitchIdsSetType
 	CreationTime                 string
 	LifecycleState               LifecycleState
 	MinSize                      int
@@ -106,6 +111,11 @@ type ScalingGroupItemType struct {
 	RemovingCapacity             int
 	RemovalPolicies              RemovalPolicySetType
 	DBInstanceIds                DBInstanceIdSetType
+	LoadBalancerIds              LoadBalancerIdSetType
+}
+
+type VSwitchIdsSetType struct {
+	VSwitchId []string
 }
 
 type RemovalPolicySetType struct {
@@ -114,6 +124,9 @@ type RemovalPolicySetType struct {
 
 type DBInstanceIdSetType struct {
 	DBInstanceId []string
+}
+type LoadBalancerIdSetType struct {
+	LoadBalancerId []string
 }
 
 // DescribeScalingGroups describes scaling groups
@@ -287,4 +300,40 @@ func (client *Client) RemoveInstances(args *RemoveInstancesArgs) (resp *RemoveIn
 		return nil, err
 	}
 	return &response, nil
+}
+
+// Default timeout value for WaitForInstance method
+const DefaultWaitTimeout = 120
+const DefaultWaitForInterval = 5
+
+// WaitForScalingGroup waits for group to given status
+func (client *Client) WaitForScalingGroup (regionId common.Region, groupId string, status LifecycleState, timeout int) error {
+	if timeout <= 0 {
+		timeout = DefaultWaitTimeout
+	}
+	for {
+		sgs, _, err := client.DescribeScalingGroups(&DescribeScalingGroupsArgs{
+			RegionId:       regionId,
+			ScalingGroupId: []string{groupId},
+		})
+		if err != nil {
+			return err
+		}
+
+		if timeout <= 0 {
+			return common.GetClientErrorFromString("Timeout")
+		}
+
+		timeout = timeout - DefaultWaitForInterval
+		time.Sleep(DefaultWaitForInterval * time.Second)
+
+		if len(sgs) < 1 {
+			return common.GetClientErrorFromString("Not found")
+		}
+		if sgs[0].LifecycleState == status {
+			break
+		}
+
+	}
+	return nil
 }
