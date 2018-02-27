ee63a3a085c6e51f1d27f001609881a8725ace59
Xiao An <hac@zju.edu.cn>
2018-02-27 15:39:08 +0800

    use 'external' as cloudprovider flag for aliyun
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- pkg/model/components/apiserver.go
+++ pkg/model/components/apiserver.go
@@ -118,7 +118,7 @@ func (b *KubeAPIServerOptionsBuilder) BuildOptions(o interface{}) error {
 	case kops.CloudProviderOpenstack:
 		c.CloudProvider = "openstack"
 	case kops.CloudProviderALI:
-		c.CloudProvider = "alicloud"
+		c.CloudProvider = "external"
 	default:
 		return fmt.Errorf("unknown cloudprovider %q", clusterSpec.CloudProvider)
 	}
